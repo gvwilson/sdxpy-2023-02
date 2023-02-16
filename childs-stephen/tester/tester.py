67c722664ade5905b280db622a048f97cc8e9d3e
Stephen Childs <sechilds@gmail.com>
2023-02-16 11:19:39 -0500

    Print test results and summary stats.
    Print results of all of the tests.
    Simple, but is not the easiest to test.

--- childs-stephen/tester/tester.py
+++ childs-stephen/tester/tester.py
@@ -1,3 +1,10 @@
+def sign(value):
+    if value < 0:
+        return -1
+    else:
+        return 1
+
+
 def test_sign_negative():
     assert sign(-3) == -1
 
@@ -29,6 +36,7 @@ def classify(func):
 
 
 def run_tests(prefix):
+    results = {"pass": 0, "fail": 0, "error": 0, "skip": 0}
     all_names = [n for n in globals() if n.startswith(prefix)]
     for name in all_names:
         func = globals()[name]
@@ -36,16 +44,23 @@ def run_tests(prefix):
         try:
             if kind == "skip":
                 print(f"skip: {name}")
+                results["skip"] += 1
             else:
                 func()
                 print(f"pass: {name}")
+                results["pass"] += 1
         except AssertionError as e:
             if kind == "fail":
                 print(f"pass (expected failure): {name}")
+                results["pass"] += 1
             else:
                 print(f"fail: {name} {str(e)}")
+                results["fail"] += 1
         except Exception as e:
             print(f"error: {name} {str(e)}")
+            results["error"] += 1
+    for result in results.keys():
+        print(f"{result} {results[result]}")
 
 
 run_tests("test_")
