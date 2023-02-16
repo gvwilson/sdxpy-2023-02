dde3cd6a62f443df041fd33f596a82737a93d487
Stephen Childs <sechilds@gmail.com>
2023-02-16 12:02:51 -0500

    Create a series of tests for self-testing.
    The tester has some tests of its own functionality.
    We create an alternate set of tests starting with test2_
    to ensure that the test runner has something to run
    when doing its test.
    
    This also ensure that the testing statistics won't change
    and we can test them.
    
    Each of these tests has a test2_ test -- and a test_ test
    that gets the result from the tester.
    
    We capture the standard output to get the text printed
    using print() -- then we compare that to the expected result.

--- childs-stephen/tester/tester.py
+++ childs-stephen/tester/tester.py
@@ -1,3 +1,7 @@
+from io import StringIO
+import sys
+
+
 def sign(value):
     if value < 0:
         return -1
@@ -25,6 +29,83 @@ test_sign_zero.fail = True
 
 def test_sign_error():
     assert sgn(1) == 1
+
+
+def test2_passing_test():
+    assert True
+
+
+def test2_failing_test():
+    assert False
+
+
+def test2_skipped_test():
+    assert True
+
+
+test2_skipped_test.skip = True
+
+
+def test2_expected_failing_test():
+    assert False
+
+
+test2_expected_failing_test.fail = True
+
+
+def test2_error_test():
+    assert 12 / 0 == 0
+
+
+def test_pass_test():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "pass: test2_passing_test" in mystdout.getvalue()
+
+
+def test_fail_test():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "fail: test2_failing_test" in mystdout.getvalue()
+
+
+def test_skip_test():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "skip: test2_skipped_test" in mystdout.getvalue()
+
+
+def test_expected_fail_test():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "pass (expected failure): test2_expected_failing_test" in mystdout.getvalue()
+
+
+def test_error_in_test():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "error: test2_error_test division by zero" in mystdout.getvalue()
+
+
+def test_results_output():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "pass 2" in mystdout.getvalue()
+    assert "fail 1" in mystdout.getvalue()
+    assert "error 1" in mystdout.getvalue()
+    assert "skip 1" in mystdout.getvalue()
 
 
 def classify(func):
