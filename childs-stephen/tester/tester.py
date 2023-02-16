de605bd95b03b78fc7dfd18bd68573c2d04d5f1a
Stephen Childs <sechilds@gmail.com>
2023-02-16 12:42:50 -0500

    Handle assert tests.
    This test uses the docstring to make tests assert tests.
    Created some assert tests to ensure that this functionality
    is working correctly in all combinations.
    
    The test totals in our test2_ tests needed to be updated.

--- childs-stephen/tester/tester.py
+++ childs-stephen/tester/tester.py
@@ -31,6 +31,50 @@ def test_sign_error():
     assert sgn(1) == 1
 
 
+def test2_raise_assert():
+    """
+    test:assert
+    """
+    assert False
+
+
+def test2_raise_assert_skip():
+    """
+    test:assert
+    """
+    assert False
+
+
+test2_raise_assert_skip.skip = True
+
+
+def test2_raise_assert_fail():
+    """
+    test:assert
+    """
+    assert True
+
+
+def test2_raise_assert_expected_fail():
+    """
+    test:assert
+    """
+    assert True
+
+
+test2_raise_assert_expected_fail.fail = True
+
+
+def test2_raise_assert_expected_fail_pass():
+    """
+    test:assert
+    """
+    assert False
+
+
+test2_raise_assert_expected_fail_pass.fail = True
+
+
 def test2_passing_test():
     assert True
 
@@ -97,22 +141,66 @@ def test_error_in_test():
     assert "error: test2_error_test division by zero" in mystdout.getvalue()
 
 
+def test_handle_assert():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "pass (assert): test2_raise_assert" in mystdout.getvalue()
+
+
+def test_handle_assert_skip():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "skip: test2_raise_assert_skip" in mystdout.getvalue()
+
+
+def test_handle_assert_fail():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert "fail (assert): test2_raise_assert_fail" in mystdout.getvalue()
+
+
+def test_handle_assert_expected_fail():
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+    run_tests("test2_")
+    sys.stdout = old_stdout
+    assert (
+        "pass (assert expected failure): test2_raise_assert_expected_fail"
+        in mystdout.getvalue()
+    )
+
+
 def test_results_output():
     old_stdout = sys.stdout
     sys.stdout = mystdout = StringIO()
     run_tests("test2_")
     sys.stdout = old_stdout
-    assert "pass 2" in mystdout.getvalue()
-    assert "fail 1" in mystdout.getvalue()
+    assert "pass 4" in mystdout.getvalue()
+    assert "fail 3" in mystdout.getvalue()
     assert "error 1" in mystdout.getvalue()
-    assert "skip 1" in mystdout.getvalue()
+    assert "skip 2" in mystdout.getvalue()
 
 
 def classify(func):
     if hasattr(func, "skip") and func.skip:
         return "skip"
     if hasattr(func, "fail") and func.fail:
-        return "fail"
+        if (
+            hasattr(func, "__doc__")
+            and func.__doc__
+            and ("test:assert" in func.__doc__)
+        ):
+            return "assert fail"
+        else:
+            return "fail"
+    if hasattr(func, "__doc__") and func.__doc__ and ("test:assert" in func.__doc__):
+        return "assert"
     return "run"
 
 
@@ -126,6 +214,14 @@ def run_tests(prefix):
             if kind == "skip":
                 print(f"skip: {name}")
                 results["skip"] += 1
+            elif kind == "assert":
+                func()
+                print(f"fail (assert): {name}")
+                results["fail"] += 1
+            elif kind == "assert fail":
+                func()
+                print(f"pass (assert expected failure): {name}")
+                results["pass"] += 1
             else:
                 func()
                 print(f"pass: {name}")
@@ -133,6 +229,9 @@ def run_tests(prefix):
         except AssertionError as e:
             if kind == "fail":
                 print(f"pass (expected failure): {name}")
+                results["pass"] += 1
+            elif kind == "assert":
+                print(f"pass (assert): {name}")
                 results["pass"] += 1
             else:
                 print(f"fail: {name} {str(e)}")
@@ -143,5 +242,10 @@ def run_tests(prefix):
     for result in results.keys():
         print(f"{result} {results[result]}")
 
+
+print("Run sample tests:")
+run_tests("test2_")
+
+print("\n\nRun actual tests:")
 
 run_tests("test_")
