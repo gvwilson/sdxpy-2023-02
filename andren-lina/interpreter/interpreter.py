6b0c16de2cc4dabc933e9684755023d7ba37263b
Lina Andrén <andren.lina@gmail.com>
2023-03-04 17:07:36 +0100

    adding definition of env_set

--- andren-lina/interpreter/interpreter.py
+++ andren-lina/interpreter/interpreter.py
@@ -1,5 +1,10 @@
 #! /usr/local/bin/python3
 
+import sys
+import json
+
+# language operations
+
 def do_add(env, args):
     assert len(args) == 2
     left = do(env, args[0])
@@ -30,31 +35,6 @@ def do_seq(env, args):
         result = do(env, item)
     return result
 
-OPS = {
-        name.replace("do_", ""): func
-        for (name, func) in globals().items()
-        if name.startswith("do_")
-}
-
-def do(env, expr):
-    # Integers evaluate to themselves
-    if isinstance(expr, int):
-        return expr
-
-    # Lists trigger function calls.
-    assert isinstance(expr, list)
-    assert expr[0] in OPS, f"Unknown operation {expr[0]}"
-    func = OPS[expr[0]]
-    return func(env, expr[1:])
-
-def env_get(env, name):
-    assert isinstance(name, str)
-    if name in env[-1]:
-        return env[1][name]
-    if name in env[0]:
-        return env[0][name]
-    assert False, f"Unknown variable {name)"
-
 def do_def(env, args):
     assert len(args) == 3
     name = args[0]
@@ -73,12 +53,55 @@ def do_call(env, args):
     func = env_get(env, name)
     assert isinstance(func, list) and (func[0] == "func")
     params, body = func[1], func[2]
-    assert len(values) == len params)
+    assert len(values) == len(params)
 
     # Run in new environment.
     env.append(dict(zip(params, values)))
     result = do(env, body)
-    env.pop())
+    env.pop()
 
     # Report.
     return result
+
+def env_get(env, name):
+    assert isinstance(name, str)
+    if name in env[-1]:
+        return env[1][name]
+    if name in env[0]:
+        return env[0][name]
+    assert False, f"Unknown variable {name}"
+
+def env_set(env, name, value):
+    assert isinstance(name, str)
+    env[name] = value
+    
+
+# dictionary of all operations defined
+
+OPS = {
+        name.replace("do_", ""): func
+        for (name, func) in globals().items()
+        if name.startswith("do_")
+}
+
+def do(env, expr):
+    # Integers evaluate to themselves
+    if isinstance(expr, int):
+        return expr
+
+    # Lists trigger function calls.
+    assert isinstance(expr, list)
+    assert expr[0] in OPS, f"Unknown operation {expr[0]}"
+    func = OPS[expr[0]]
+    return func(env, expr[1:])
+
+
+def main():
+    assert len(sys.argv) == 2, "Usage: expr.py filename"
+    with open(sys.argv[1], "r") as reader:
+        program = json.load(reader)
+    result = do({}, program)
+    print(f"=> {result}")
+
+if __name__ == "__main__":
+    main()
