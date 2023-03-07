6b4db2712ceef09f917fd5a77217a4351de6f3e0
Stephen Childs <sechilds@gmail.com>
2023-03-07 11:35:36 -0500

    File history WIP

--- childs-stephen/backup/file-history.py
+++ childs-stephen/backup/file-history.py
@@ -21,31 +21,44 @@ def read_manifest(path):
 def read_all_manifests(manifest_list):
     return [read_manifest(p) for p in manifest_list]
 
+
 def file_history(manifest_data, filename):
     result = []
     current_filename = filename
     num_manifiests = len(manifest_data)
-    last_index = num_manifiests -1
+    last_index = num_manifiests - 1
     for i in range(last_index, 1, -1):
         if current_filename in manifest_data[i].keys():
-            #contents changed
-            old_content = manifest_data[i - 1][current_filename] 
+            # contents changed
+            old_content = manifest_data[i - 1][current_filename]
             new_content = manifest_data[i][current_filename]
             if old_content != new_content:
-                result.append({"event": "contents changed", "old": old_content, "new": new_content})
+                result.append(
+                    {
+                        "event": "contents changed",
+                        "old": old_content,
+                        "new": new_content,
+                    }
+                )
             else:
                 old_manifest = manifest_data[i - 1]
-                found = list(old_manifest.keys())[list(old_manifest.values()).index(new_content)]
+                found = list(old_manifest.keys())[
+                    list(old_manifest.values()).index(new_content)
+                ]
                 if not found:
-                    result.append("event": "added", "old": "", "new": current_filename)
-                    #stop checking history
+                    result.append(
+                        {"event": "added", "old": "", "new": current_filename}
+                    )
+                    # stop checking history
                     break
                 elif found != current_filename:
-                    result.append({"event": "rename", "old": found, "new": current_filename})
+                    result.append(
+                        {"event": "rename", "old": found, "new": current_filename}
+                    )
                     current_filename = found
                 else:
-                    #no change
-                    pass 
+                    # no change
+                    pass
         else:
             # file not in current state
             break
