47033710b5001e091ecfa57508376c33e5f97e3a
Jonathan Guyer <guyer@nist.gov>
2023-03-08 09:45:43 -0500

    Test read_manifest()

--- guyer-jonathan/backup/compare-manifests.py
+++ guyer-jonathan/backup/compare-manifests.py
@@ -1,14 +1,20 @@
 import csv
 
-def read_manifest(manifestfile):
+def read_manifest(manifest_file):
     manifest = {}
     with open(manifest_file, mode="r") as csvfile:
         manifest_reader = csv.reader(csvfile, delimiter=",")
         for filehash, filename in manifest_reader:
             manifest[filehash] = filename
-        
+
+    return manifest
+
 def compare_manifests(fileA, fileB):
     pass
 
 if __name__ == "__main__":
-    compare_manifests(fileA, fileB)
+    # compare_manifests(fileA, fileB)
+    manifestA = read_manifest("test/manifestA.csv")
+    print(manifestA)
+    manifestB = read_manifest("test/manifestB.csv")
+    print(manifestB)
