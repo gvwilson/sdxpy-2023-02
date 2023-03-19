f2852a4bbd006c197e555ab4d8c737b67a26138f
Stephen Childs <sechilds@gmail.com>
2023-03-19 07:45:56 -0400

    Tests from both directions.

--- childs-stephen/binary/bitwise.py
+++ childs-stephen/binary/bitwise.py
@@ -9,7 +9,7 @@ def to_binary(input):
         last_bit = input & 0b1
         output = str(last_bit) + output
         input = input >> 1
-    return output
+    return "0b" + output
 
 
 # Write another function that converts a string of 1's and 0's into an integer
@@ -18,6 +18,8 @@ def to_binary(input):
 
 # we set the last bit using or
 def to_integer(input):
+    if input[0:1] == "0b":
+        input = input[2:]
     output = 0b0
     for c in input:
         i = int(c)
@@ -38,5 +40,10 @@ def roundtrip2(string):
 TESTS = [0, 8, 9, 100, 1001, 998, 9989]
 
 for i in TESTS:
-    print(f"integer: {i}\tbinary:{to_binary(i)}")
+    print(f"decimal: {i}\t\t\tbinary:{to_binary(i)}")
     assert roundtrip1(i)
+
+for i in TESTS:
+    str_i = bin(i)
+    print(f"binary: {str_i}\t\t\tdecimal:{to_integer(str_i)}")
+    assert roundtrip2(str_i)
