9668f2747d0b686d385e91588b466d81ae78ce44
Stephen Childs <sechilds@gmail.com>
2023-03-19 06:47:26 -0400

    Refactored png check.

--- childs-stephen/binary/check_png.py
+++ childs-stephen/binary/check_png.py
@@ -1,15 +1,22 @@
-# The first eight bytes of a PNG image file always contain the following (base-10) values:
-# 137 80 78 71 13 10 26 10
-# Write a program that determines whether a file is a PNG image or not.
-
-decimal_bytes = [137, 80, 78, 71, 13, 10, 26, 10]
-hex_bytes = [hex(i) for i in decimal_bytes]
-print(hex_bytes)
+import sys
 
 png_bytes = b"\x89\x50\x4e\x47\x0d\x0a\x1a\x0a"
 
-with open("ZoomBG_Winter4.png", "rb") as f:
-    contents = f.read()
-    print(contents[:8])
-    if contents[:8] == png_bytes:
-        print("PNG file detected")
+
+def is_png(content):
+    return content[:8] == png_bytes
+
+
+def png_check_file(filename):
+    with open(filename, "rb") as f:
+        contents = f.read()
+        return is_png(contents)
+
+
+assert png_check_file("ZoomBG_Winter4.png"), "false negative"
+assert png_check_file("README.md") == False, "false positive"
+
+files = sys.argv[1:]
+
+for f in files:
+    print(f"{f}...{png_check_file(f)}")
