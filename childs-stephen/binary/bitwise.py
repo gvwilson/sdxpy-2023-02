495caf195265186091b4729a6ca404d950ea522f
Stephen Childs <sechilds@gmail.com>
2023-03-19 07:40:09 -0400

    Convert to functions.

--- childs-stephen/binary/bitwise.py
+++ childs-stephen/binary/bitwise.py
@@ -4,13 +4,15 @@
 input = 9
 print(bin(input))
 
-output = ""
 
-while input > 0:
-    last_bit = input & 0b1
-    output = str(last_bit) + output
-    input = input >> 1
-print(output)
+def to_binary(input):
+    output = ""
+
+    while input > 0:
+        last_bit = input & 0b1
+        output = str(last_bit) + output
+        input = input >> 1
+    return output
 
 
 # Write another function that converts a string of 1's and 0's into an integer
@@ -23,17 +25,33 @@ print(0b100101)
 print(f"first character as integer: {int(input2[0])}")
 print(f"first character as binary: {bin(int(input2[0]))}")
 
-output2 = 0b0
 
 # we don't actually need to do it in reverse
 for c in reversed(input2):
     print(c)
 
-# we set the last bit using or
-for c in input2:
-    i = int(c)
-    # we do the shift first -- for the first bit the output is already zero
-    output2 = output2 << 1
-    output2 = output2 | i
 
-print(output2)
+# we set the last bit using or
+def to_integer(input):
+    output = 0b0
+    for c in input:
+        i = int(c)
+        # we do the shift first -- for the first bit the output is already zero
+        output = output << 1
+        output = output | i
+    return output
+
+
+def roundtrip1(integer):
+    return integer == to_integer(to_binary(integer))
+
+
+def roundtrip2(string):
+    return string == to_binary(to_integer(string))
+
+
+TESTS = [0, 8, 9, 100, 1001, 998, 9989]
+
+for i in TESTS:
+    print(f"integer: {i}\tbinary:{to_binary(i)}")
+    assert roundtrip1(i)
