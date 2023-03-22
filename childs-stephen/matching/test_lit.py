1fb602375112ff707581dbde174e21eecd4c30f3
Stephen Childs <sechilds@gmail.com>
2023-03-22 18:47:39 -0400

    test literal matching.

---
+++ childs-stephen/matching/test_lit.py
@@ -0,0 +1,13 @@
+from match import Lit
+
+
+def test_literal_match_entire_string():
+    assert Lib("abc").match("abc")
+
+
+def test_literal_substring_alone_no_match():
+    assert not Lit("ab").match("abc")
+
+
+def test_literal_superstring_no_match():
+    assert not Lit("abc").match("ab")
