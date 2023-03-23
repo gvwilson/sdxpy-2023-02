2d21f84696bee79cf3ba1963188d0f0f8352aa44
Jonathan Guyer <guyer@nist.gov>
2023-03-23 00:06:07 -0400

    Add edge cases

--- guyer-jonathan/binary/test_int2bits.py
+++ guyer-jonathan/binary/test_int2bits.py
@@ -1,7 +1,13 @@
 from int2bits import int2bits, bits2int
 
-def test_int2bits():
+def test_10_2bits():
     assert int2bits(10) == "1010"
+
+def test_0_2bits():
+    assert int2bits(0) == "0"
+
+def test_65535_2bits():
+    assert int2bits(65535) == "1111111111111111"
 
 def test_bits2int():
     pass
