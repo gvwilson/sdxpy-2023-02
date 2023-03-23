13eb369b4b34a32eb98b195e897d861423c7884e
Jonathan Guyer <guyer@nist.gov>
2023-03-22 23:30:04 -0400

    Add PNG checker and tests

---
+++ guyer-jonathan/binary/check_png.py
@@ -0,0 +1,10 @@
+PNG_magic = [137, 80, 78, 71, 13, 10, 26, 10]
+
+def is_png(path):
+    with open(path, mode='rb') as f:
+        result = True
+        for magic_char in PNG_magic:
+            char = f.read(1)
+            result &= (ord(char) == magic_char)
+        
+    return result
