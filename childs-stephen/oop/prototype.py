a95216b06ef7c9f69bddb16afaf895a25796fcfc
Stephen Childs <sechilds@gmail.com>
2023-04-01 22:06:09 -0400

    Prototype - and answer to the question.

--- childs-stephen/oop/prototype.py
+++ childs-stephen/oop/prototype.py
@@ -51,3 +51,16 @@ square["side"] = 3
 print(
     f"{square['name']}: area: {call(square, 'area')} perimeter: {call(square, 'perimeter')} density: {call(square, 'density', 5)}"
 )
+
+red_square = clone(square)
+red_square["name"] = "rsq"
+red_square["colour"] = "red"
+
+print(
+    f"{red_square['name']}: area: {call(red_square, 'area')} perimeter: {call(red_square, 'perimeter')} density: {call(red_square, 'density', 5)} colour: {red_square['colour']}"
+)
+
+# I think prototypes are a bit harder to understand, but I'm much more
+# familiar with classes than prototypes.
+# It does seem to be a potential source of confusion that you clone
+# the data along with the methods. Classes force you to change that data.
