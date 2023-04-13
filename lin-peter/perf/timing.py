94e159a70276a867dd0dc4955a2013fdcf690d9f
peter0083 <peter.tingyao@gmail.com>
2023-04-13 12:56:31 -0700

    make faster select in df_col2

--- lin-peter/perf/timing.py
+++ lin-peter/perf/timing.py
@@ -4,6 +4,7 @@ import time
 import datetime
 
 from df_col import DfCol
+from df_col2 import DfCol2
 from df_row import DfRow
 
 RANGE = 10
@@ -14,6 +15,13 @@ def make_col(nrow, ncol):
         return [((start + i) % RANGE) for i in range(n)]
     fill = {f"label_{c}": _col(nrow, c) for c in range(ncol)}
     return DfCol(**fill)
+
+def make_col2(nrow, ncol):
+    """Make a column-wise dataframe ver2 for profiling."""
+    def _col(n, start):
+        return [((start + i) % RANGE) for i in range(n)]
+    fill = {f"label_{c}": _col(nrow, c) for c in range(ncol)}
+    return DfCol2(**fill)
 
 def make_row(nrow, ncol):
     """Make a row-wise dataframe for profiling."""
@@ -51,10 +59,13 @@ def sweep(sizes):
     result = []
     for (nrow, ncol) in sizes:
         df_col = make_col(nrow, ncol)
+        df_col2 = make_col2(nrow, ncol)
         df_row = make_row(nrow, ncol)
         times = [
             time_filter(df_col),
+            time_filter(df_col2),
             time_select(df_col),
+            time_select(df_col2),
             time_filter(df_row),
             time_select(df_row),
         ]
@@ -75,7 +86,7 @@ def report(result):
     with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerow(
-            ["nrow", "ncol", "filter_col", "select_col", "filter_row", "select_row"]
+            ["nrow", "ncol", "filter_col", "filter_col2", "select_col", "select_col2", "filter_row", "select_row"]
         )
         for row in result:
             writer.writerow(row)
